9edb10a6a9b9e68454bb37816f6ebde086225793
zeg064 <50365152+zeg064@users.noreply.github.com>
2025-01-07 01:14:45 +1000

    Update climate.py
    https://developers.home-assistant.io/blog/2024/03/13/deprecate_add_run_job/

--- custom_components/intesisbox/climate.py
+++ custom_components/intesisbox/climate.py
@@ -241,12 +241,12 @@ class IntesisBoxAC(ClimateEntity):
             if self._target_temperature:
                 self._controller.set_temperature(self._target_temperature)
 
-        self.hass.async_create_task(self.schedule_update_ha_state, False)
+        self.hass.async_add_executor_job(self.schedule_update_ha_state, False)
 
     def turn_on(self):
         """Turn thermostat on."""
         self._controller.set_power_on()
-        self.hass.async_create_task(self.schedule_update_ha_state, False)
+        self.hass.async_add_executor_job(self.schedule_update_ha_state, False)
 
     def turn_off(self):
         """Turn thermostat off."""
@@ -329,7 +329,7 @@ class IntesisBoxAC(ClimateEntity):
         """Let HA know there has been an update from the controller."""
         _LOGGER.debug("Intesisbox sent a status update.")
         if self.hass:
-            self.hass.async_create_task(self.schedule_update_ha_state, True)
+            self.hass.async_add_executor_job(self.schedule_update_ha_state, True)
 
     @property
     def min_temp(self):
